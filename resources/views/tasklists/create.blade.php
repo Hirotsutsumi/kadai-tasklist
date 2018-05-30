8f6cbd034199a833532da81e07706208cd2a5bbf
hiro.tsutsumi <ec2-user@ip-172-31-18-142.us-east-2.compute.internal>
2018-05-30 01:14:07 +0000

    add column

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -3,8 +3,11 @@
 @section('content')
 
     <h1>タスク新規作成ページ</h1>
-
+    
     {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
+    
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
 
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
@@ -12,8 +15,5 @@
         {!! Form::submit('作成') !!}
 
     {!! Form::close() !!}
-    
-    {{ '<p style="color: red;">When passed through htmlentities function</p>' }}
-    {!! '<p style="color: red;">When not passed through htmlentities function</p>' !!}
 
 @endsection
